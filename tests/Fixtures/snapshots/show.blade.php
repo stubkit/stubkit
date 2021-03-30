b88a759fb86f11817f9971a3aca50dc42cea8136
Brian Dillingham <brian@dillingham.dev>
2021-03-30 03:52:40 -0400

    Update snapshots

--- tests/Fixtures/snapshots/show.blade.php
+++ tests/Fixtures/snapshots/show.blade.php
@@ -1,14 +1,12 @@
-<table>
-    <tr>
-        <td>Id</td>
-        <td>{{ $post->id }}</td>
-    </tr>
-    <tr>
-        <td>First Name</td>
-        <td>{{ $post->first_name }}</td>
-    </tr>
-    <tr>
-        <td>Last Name</td>
-        <td>{{ $post->last_name }}</td>
-    </tr>
-</table>
+<tr>
+    <td>Id</td>
+    <td>{{ $post->id }}</td>
+</tr>
+<tr>
+    <td>First Name</td>
+    <td>{{ $post->first_name }}</td>
+</tr>
+<tr>
+    <td>Last Name</td>
+    <td>{{ $post->last_name }}</td>
+</tr>
